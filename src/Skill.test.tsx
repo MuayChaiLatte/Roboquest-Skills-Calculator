724b981afe986aa46c2edf57c754448c93989acc
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-07-29 12:32:52 +0100

    renamed test data variable

--- src/Skill.test.tsx
+++ src/Skill.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Skill from './Skill';
 
 // Making test data
-const mockSkillsAvailableToThisClass: { name: string, description: string } = {
+const mockSkillAvailableToThisClass: { name: string, description: string } = {
     name: 'Stim Bubble',
     description: 'While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'
 }
@@ -11,10 +11,10 @@ const mockSkillsAvailableToThisClass: { name: string, description: string } = {
 test("Skill renders successfully", () => {
     
     // Render Component
-    render(<Skill name={mockSkillsAvailableToThisClass.name} description={mockSkillsAvailableToThisClass.description} />);
+    render(<Skill name={mockSkillAvailableToThisClass.name} description={mockSkillAvailableToThisClass.description} />);
 
     // Grab element containing skill text based on if it matches "placeholder skill", ignoring case
-    const element = screen.getByText(mockSkillsAvailableToThisClass.description);
+    const element = screen.getByText(mockSkillAvailableToThisClass.description);
     
     // Assertion: element exists and contains correct text
     expect(element).toBeInTheDocument
