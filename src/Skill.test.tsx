9f1f7c8be0f9e25c41e2e338d9f6a686db518e4f
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-07-29 11:32:37 +0100

    Skill Test now checks whether an element exists with the correct skill description

--- src/Skill.test.tsx
+++ src/Skill.test.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Skill from './Skill';
 
+// Making test data
+const mockSkillsAvailableToThisClass: { name: string, description: string } = {
+    name: 'Stim Bubble',
+    description: 'While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'
+}
+
 test("Skill renders successfully", () => {
     
     // Render Component
     render(<Skill/>);
 
     // Grab element containing skill text based on if it matches "placeholder skill", ignoring case
-    const element = screen.getByText(/placeholder skill/i);
+    const element = screen.getByText(mockSkillsAvailableToThisClass.description);
     
     // Assertion: element exists and contains correct text
     expect(element).toBeInTheDocument
