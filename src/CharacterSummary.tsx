2f7e911e54eda4ab595354ca71e4d73b270c5be8
MuayChaiLatte <115982939+MuayChaiLatte@users.noreply.github.com>
2024-07-31 17:28:06 +0100

    Created CharacterSummary Component

---
+++ src/CharacterSummary.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CharacterSummary() {
+  return (
+    <div className="CharacterSummary">
+      <p>Character Name</p>
+      <p>Level: 1</p>
+    </div>
+  );
+}
+
+export default CharacterSummary;
