9943565e03bbeb9b644172cd0cd4208f71a510df
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-08-06 16:39:59 +0100

    Lifted state up from CharacterSummary, characterLevel is now incremented or decremented depending on if a skill is being enabled or disabled

--- src/SkillTree.tsx
+++ src/SkillTree.tsx
@@ -7,16 +7,19 @@ import Skill from './Skill';
 function SkillTree() {
     const totalNumberOfSkillsAvailableToClass = 1
     const [skillsSelected, setSkillsSelected] = useState(Array(totalNumberOfSkillsAvailableToClass).fill(false))
+    const [characterLevel, setCharacterLevel] = useState(1)
+
 
     function handleSkillClick() {
-        const nextSkillsSelected = skillsSelected.slice()
-        nextSkillsSelected[0] = !nextSkillsSelected[0]
-        setSkillsSelected(nextSkillsSelected)
+        const nextSkillsSelected = skillsSelected.slice();
+        nextSkillsSelected[0] = !nextSkillsSelected[0];
+        setSkillsSelected(nextSkillsSelected);
+        nextSkillsSelected[0] ? setCharacterLevel(characterLevel + 1) : setCharacterLevel(characterLevel - 1);
     }
 
     return (
         <div>
-            <CharacterSummary characterName='Guardian'/>
+            <CharacterSummary characterName='Guardian' characterLevel={characterLevel}/>
             <Skill 
             name='Stim Bubble' 
             description='While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'
