4187dca7571dffd021f7499d1e711cb36adedde5
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-08-06 13:38:48 +0100

    Lifted state up from Skill, SkillTree now tracks which skills have been selected and passes click handler to Skill so that Skill can control Skill's parent state

--- src/SkillTree.tsx
+++ src/SkillTree.tsx
@@ -5,10 +5,24 @@ import Skill from './Skill';
 
 
 function SkillTree() {
+    const totalNumberOfSkillsAvailableToClass = 1
+    const [skillsSelected, setSkillsSelected] = useState(Array(totalNumberOfSkillsAvailableToClass).fill(false))
+
+    function handleSkillClick() {
+        const nextSkillsSelected = skillsSelected.slice()
+        nextSkillsSelected[0] = !nextSkillsSelected[0]
+        setSkillsSelected(nextSkillsSelected)
+    }
+
     return (
         <div>
             <CharacterSummary characterName='Guardian'/>
-            <Skill name='Stim Bubble' description='While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'/>
+            <Skill 
+            name='Stim Bubble' 
+            description='While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'
+            isSkillSelected={skillsSelected[0]}
+            onSkillClick={handleSkillClick}
+            />
         </div>
     )
 }
