1d592b2694844170b814fe267837795cbd847a8f
Jey Ashokkumaar <jeyashokkumaar@gmail.com>
2024-08-08 15:35:25 +0100

    Created new unit test for CharacterClassDisplay - Tests selecting and deselecting every Perk rendered and that characterLevel is correctly changing accordingly

---
+++ src/CharacterClassDisplay.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { CharacterClassDisplay, CharacterClassDisplayProps } from './CharacterClassDisplay';
+import userEvent from '@testing-library/user-event';
+
+// Making test data
+const mockCharacterClassDisplayProps: CharacterClassDisplayProps = {
+    characterClassName: "Guardian",
+    allCharacterClassPerks: [
+        {
+            perkName: 'Stim Bubble',
+            perkDescription: 'While Bastion is active, increases weapons damage by 30%, firerate by 30% and movement speed by 18%.'
+        },
+        {
+            perkName: 'Safe Space',
+            perkDescription: 'Reduces Bastion cooldown by 10%. While Bastion is active, repairs 5% of your scratch damage every 1s.'
+        }
+    ]
+}
+
+test("Selecting each available Perk consecutively increases characterClassLevel correctly by 1 each time", async () => {
+    
+    const user = userEvent.setup();
+
+    // Render Component
+    render(<CharacterClassDisplay {...mockCharacterClassDisplayProps} />);
+
+    // Grab all buttons to be clicked
+    const buttons = screen.getAllByText('Select', {selector: 'button'});
+    const characterClassLevel = screen.getByText(/Level: /);
+
+    // Grab every button 
+    for (let i=0; i < buttons.length; i++) {
+        await user.click(buttons[i]);
+        expect(characterClassLevel).toHaveTextContent(`Level: ${i+2}`);
+    }
+
+})
+
+test("Each Perk increments characterLevel when selected then decrements characterLevel when deselected", async () => {
+    
+    const user = userEvent.setup();
+
+    // Render Component
+    render(<CharacterClassDisplay {...mockCharacterClassDisplayProps} />);
+
+    // Grab all buttons to be clicked
+    const buttons = screen.getAllByText('Select', {selector: 'button'});
+    const characterClassLevel = screen.getByText(/Level: /);
+
+    // Grab every button 
+    for (let i=0; i < buttons.length; i++) {
+        await user.click(buttons[i]);
+        expect(characterClassLevel).toHaveTextContent(`Level: ${i+2}`);
+    }
+
+})
